cffdb7621ce92231a84b87a56ef4d9229e058014
Kaickpl <kaick.mlopes@upe.br>
2026-03-29 14:25:07 -0300

    primeiro commit

---
+++ src/main/java/br/com/upe/espectrum/EspectrumApplication.java
@@ -0,0 +1,13 @@
+package br.com.upe.espectrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspectrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspectrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/upe/espectrum/EspectrumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.upe.espectrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspectrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
