6e7affb70843565b77374f9b710477a3803c6107
Kaickpl <kaick.mlopes@upe.br>
2026-06-27 20:43:15 -0300

    ajuste

--- src/main/java/br/com/upe/espectrum/dto/requestDtos/TrocarSenhaDto.java
+++ src/main/java/br/com/upe/espectrum/dto/requestDtos/TrocarSenhaDto.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class TrocarSenhaDto {
     private String email;
+    private String codigo;
     private String novaSenha;
     private String confirmaSenha;
 
